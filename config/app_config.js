c0e78f0ba301eeab4c17433c7004e00030d9063b
Sam Messina <sam@smessina.com>
2018-09-15 17:33:46 -0500

    change port to 3000

--- config/app_config.js
+++ config/app_config.js
@@ -7,7 +7,7 @@ const expressStatusMonitor = require('express-status-monitor');
 module.exports = (app) => {
   // Express configuration
   app.set('host', process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0');
-  app.set('port', process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080);
+  app.set('port', process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 3000);
 
   app.use(expressStatusMonitor());
 
