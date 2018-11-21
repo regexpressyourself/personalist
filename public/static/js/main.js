ca897f8470a538e7efec06e1f6c0b545fdc04521
Sam <samuel.messina@siu.edu>
2018-11-21 14:10:29 -0600

    add description color and save notification

--- public/static/js/main.js
+++ public/static/js/main.js
@@ -8,14 +8,14 @@ function httpGetAsync(theUrl, callback) { //theURL or a path to file
   var httpRequest = new XMLHttpRequest();
   httpRequest.onreadystatechange = function() {
     if (httpRequest.readyState == 4 && httpRequest.status == 200) {
-      var data = httpRequest.responseText;  //if you fetch a file you can JSON.parse(httpRequest.responseText)
+      var data = httpRequest.responseText; //if you fetch a file you can JSON.parse(httpRequest.responseText)
       if (callback) {
         callback(data);
-      }                   
+      }
     }
   };
 
-  httpRequest.open('GET', theUrl, true); 
+  httpRequest.open('GET', theUrl, true);
   httpRequest.send(null);
 }
 
@@ -24,16 +24,18 @@ function httpPostAsync(theUrl, payload, callback) { //theURL or a path to file
   var httpRequest = new XMLHttpRequest();
   httpRequest.onreadystatechange = function() {
     if (httpRequest.readyState == 4 && httpRequest.status == 200) {
-      var data = httpRequest.responseText;  //if you fetch a file you can JSON.parse(httpRequest.responseText)
+      var data = httpRequest.responseText; //if you fetch a file you can JSON.parse(httpRequest.responseText)
       if (callback) {
         callback(data);
-      }                   
+      }
     }
   };
 
-  httpRequest.open('POST', theUrl, true); 
+  httpRequest.open('POST', theUrl, true);
   httpRequest.setRequestHeader('Content-Type', 'application/json');
-  httpRequest.send(JSON.stringify({payload: payload}));
+  httpRequest.send(JSON.stringify({
+    payload: payload
+  }));
 }
 
 const kendall_user = 'kwatch90210';
@@ -43,7 +45,7 @@ const sam_user = 'smessina';
 
 
 let getUserPlaylists = (user) => {
-  switch(user) {
+  switch (user) {
     case 'sam':
       user = sam_user;
       break;
@@ -59,7 +61,9 @@ let getUserPlaylists = (user) => {
 
 let updateUserPlaylistsView = () => {
   httpGetAsync(`/playlists?user=${state.user}`, function(data) {
-    if (data === 'try again') {updateUserPlaylistsView();}
+    if (data === 'try again') {
+      updateUserPlaylistsView();
+    }
     let items = [];
     data = JSON.parse(data).items;
 
@@ -104,14 +108,18 @@ let updatePlaylistSongsView = () => {
     let items = [];
     data = JSON.parse(data);
     for (let song of data['items']) {
+      let hasDescription = '';
       let newData = {
         // the special id for playlist+song combination. 
         // this is the key in the db for the song. the value is the description
         "messinaID": song.messinaId,
         "description": song.description
       }
+      if (song.description.length > 0) {
+        hasDescription = 'has-description';
+      }
       items.push(`
-      <div class="song-item song-item--${song.id}">
+      <div class="song-item song-item--${song.id} ${hasDescription}">
         <p onclick="toggleDescription(event, '${song.id}')" class="song-item__name song-item__name--${song.id} clickable">
           <span>
             ${song.name}
@@ -142,15 +150,18 @@ let setPlaylistSongs = (id) => {
   let saveBtn = document.getElementById(`save-btn-${id}`);
   let cancelBtn = document.getElementById(`cancel-btn-${id}`);
   let newDescription = document.getElementById(`song-${id}`).value;
-  let payload = [{ 'id': id, 'description': newDescription }];
+  let payload = [{
+    'id': id,
+    'description': newDescription
+  }];
 
-  httpPostAsync('/playlist', payload, (data) => { 
-    saveBtn.classList.add('animorph');
-    cancelBtn.classList.add('animorph');
+  httpPostAsync('/playlist', payload, (data) => {
+    let saveConfirmation = document.querySelector('.save-confirmation');
+    saveConfirmation.classList.add('save-confirmation--on');
+
     setTimeout(() => {
-      saveBtn.classList.remove('animorph');
-      cancelBtn.classList.remove('animorph');
-    }, 2000);
+      saveConfirmation.classList.remove('save-confirmation--on');
+    }, 3000);
   });
 }
 
@@ -160,13 +171,12 @@ let updateMainView = (content) => {
 }
 
 let toggleDescription = (event, songId) => {
-	if (event.target.closest('.song-item__name__play')) return;
+  if (event.target.closest('.song-item__name__play')) return;
   let songEl = document.querySelectorAll(`.song-item--${songId}`)[0];
   if (songEl.classList.contains('song-item--shown')) {
     songEl.classList.add('song-item--hidden');
     songEl.classList.remove('song-item--shown');
-  }
-  else {
+  } else {
     songEl.classList.add('song-item--shown');
     songEl.classList.remove('song-item--hidden');
   }
@@ -178,11 +188,11 @@ let playSong = (songId) => {
   document.querySelectorAll(`#play-icon--${songId}`)[0].onclick = () => pauseSong(songId);
   document.querySelectorAll(`#play-icon--${songId}`)[0].classList.add('play-icon--playing');
   setTimeout(() => {
-  document.querySelectorAll(`#play-icon--${songId}`)[0].innerHTML = `
+    document.querySelectorAll(`#play-icon--${songId}`)[0].innerHTML = `
 <svg id="pause-icon-${songId}" class="feather feather-x sc-dnqmqq jxshSx" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true" data-reactid="1336"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
   `;
-  document.querySelectorAll(`#play-icon--${songId}`)[0].onclick = () => pauseSong(songId);
-  document.querySelectorAll(`#play-icon--${songId}`)[0].style.opacity = 1;
+    document.querySelectorAll(`#play-icon--${songId}`)[0].onclick = () => pauseSong(songId);
+    document.querySelectorAll(`#play-icon--${songId}`)[0].style.opacity = 1;
   }, 500);
 }
 
@@ -205,7 +215,7 @@ function findGetParameter(parameterName) {
   location.search
     .substr(1)
     .split("&")
-    .forEach(function (item) {
+    .forEach(function(item) {
       tmp = item.split("=");
       if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
     });
