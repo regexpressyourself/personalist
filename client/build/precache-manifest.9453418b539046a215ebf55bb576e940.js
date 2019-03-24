85b113085527ea23969abde83a68a6f0effe8b8a
Sam <samuel.messina@siu.edu>
2019-03-24 14:37:52 -0500

    add build to source control and update favicon

---
+++ client/build/precache-manifest.9453418b539046a215ebf55bb576e940.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2312fca1e81956b7deb",
+    "url": "/static/css/main.77d4bb9e.chunk.css"
+  },
+  {
+    "revision": "b2312fca1e81956b7deb",
+    "url": "/static/js/main.cd63c82b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8bb9abba9fd1589858ff",
+    "url": "/static/js/2.8202e772.chunk.js"
+  },
+  {
+    "revision": "6716f3ecd78959ab589ef7fc9e6e68c9",
+    "url": "/static/media/personalist-orange.6716f3ec.png"
+  },
+  {
+    "revision": "91d10d56e79b16d89f9b507846cc5deb",
+    "url": "/static/media/personalist-white.91d10d56.png"
+  },
+  {
+    "revision": "dcb1e9df56df562253cb5b65ab5807c8",
+    "url": "/index.html"
+  }
+];
