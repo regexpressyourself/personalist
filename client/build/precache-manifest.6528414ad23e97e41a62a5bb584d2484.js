7a754b94f16a8990778800855b06e556a4c5174c
Sam Messina <samuel.messina@gmail.com>
2022-09-10 18:52:14 +0000

    update

---
+++ client/build/precache-manifest.6528414ad23e97e41a62a5bb584d2484.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "91d10d56e79b16d89f9b507846cc5deb",
+    "url": "/static/media/personalist-white.91d10d56.png"
+  },
+  {
+    "revision": "6716f3ecd78959ab589ef7fc9e6e68c9",
+    "url": "/static/media/personalist-orange.6716f3ec.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4c5b4b70458b70d05196",
+    "url": "/static/js/main.78be1f72.chunk.js"
+  },
+  {
+    "revision": "3d0b27275a6ba1428296",
+    "url": "/static/js/2.a18752c9.chunk.js"
+  },
+  {
+    "revision": "4c5b4b70458b70d05196",
+    "url": "/static/css/main.a5a19289.chunk.css"
+  },
+  {
+    "revision": "5f51f5febcb09f0aba6944b2d79e3a20",
+    "url": "/index.html"
+  }
+];
--- config/app_config.js
+++ config/app_config.js
@@ -10,7 +10,7 @@ module.exports = (app) => {
 
   // Express configuration
   app.set('host', process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0');
-  app.set('port', process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 3000);
+  app.set('port', process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 3012);
 
 
   app.use(expressStatusMonitor());
