86b3c32bad6fc03cdc523a92b2ee272ccade4b82
Sam Messina <samuel.messina@gmail.com>
2022-09-10 18:24:27 +0000

    update

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9453418b539046a215ebf55bb576e940.js"
+  "/precache-manifest.80752137b2af75d2b4484d4e396ca372.js"
 );
 
 workbox.clientsClaim();
